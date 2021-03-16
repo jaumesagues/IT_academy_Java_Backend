3ad86d93f46610a5ed6e991e0af7bb7f2c4045df
jaumesagues <jaumesagues@yahoo.es>
2021-03-16 22:09:33 +0100

    Correcció exercici nivell 1 fase 4 del módul 2

--- Repositorio/M2/Nivell1Fase4App.java
+++ Repositorio/M2/Nivell1Fase4App.java
@@ -1,32 +1,33 @@
 import java.awt.List;
+import java.util.ArrayList;
 
 public class Nivell1Fase4App {
 
 	public static void main(String[] args) {
-		List nom=new List(5);
-		List cognom=new List(6);
-		List nom_complet=new List();
-		nom.add("J");
-		nom.add("a");
-		nom.add("u");
-		nom.add("m");
-		nom.add("e");
-		cognom.add("S");
-		cognom.add("a");
-		cognom.add("g");
-		cognom.add("u");
-		cognom.add("e");
-		cognom.add("s");
+		ArrayList<Character> nom=new ArrayList<Character>(5);
+		ArrayList<Character> cognom=new ArrayList<Character>(6);
+		ArrayList<Character> nom_complet=new ArrayList<Character>();
+		nom.add('J');
+		nom.add('a');
+		nom.add('u');
+		nom.add('m');
+		nom.add('e');
+		cognom.add('S');
+		cognom.add('a');
+		cognom.add('g');
+		cognom.add('u');
+		cognom.add('e');
+		cognom.add('s');
 		
-		for (int i=0; i<nom.getItemCount(); i++) {
-			nom_complet.add(nom.getItem(i));
+		for (int i=0; i<nom.size(); i++) {
+			nom_complet.add(nom.get(i));
 		}
-		nom_complet.add(" ");
-		for (int i=0; i<cognom.getItemCount(); i++) {
-			nom_complet.add(cognom.getItem(i));
+		nom_complet.add(' ');
+		for (int i=0; i<cognom.size(); i++) {
+			nom_complet.add(cognom.get(i));
 		}
-		for (int i=0; i<nom_complet.getItemCount();i++) {
-		 System.out.print(nom_complet.getItem(i));
+		for (int i=0; i<nom_complet.size();i++) {
+		 System.out.print(nom_complet.get(i));
 		}
 
 	}
