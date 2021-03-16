6f88f9386c71c587974570603d4b1a7c4078b6b9
jaumesagues <jaumesagues@yahoo.es>
2021-03-16 22:03:57 +0100

    Correccions al exercici nivell 1 fase 2 del modul M2

--- Repositorio/M2/Nivell1Fase2App.java
+++ Repositorio/M2/Nivell1Fase2App.java
@@ -1,24 +1,29 @@
 
 import java.awt.*;
+import java.util.ArrayList;
 public class Nivell1Fase2App {
 
 	public static void main(String[] args) {
-		List nom=new List(5);
-		nom.add("J");
-		nom.add("a");
-		nom.add("u");
-		nom.add("m");
-		nom.add("e");
+		ArrayList<Character> nom=new ArrayList<Character>();
+		nom.add('J');
+		nom.add('a');
+		nom.add('u');
+		nom.add('1');
+		nom.add('e');
 		
-		for (int i=0; i<nom.getItemCount(); i++) {
-			String lletra=nom.getItem(i);
+		for (int i=0; i<nom.size(); i++) {
+			Character lletra=nom.get(i);
 			
 			System.out.print(lletra);
-			if (lletra.toLowerCase().matches("[aeiou]")) {
+			if (Character.toLowerCase(lletra)=='a' || 
+					Character.toLowerCase(lletra)=='e' ||
+					Character.toLowerCase(lletra)=='i' ||
+					Character.toLowerCase(lletra)=='o' ||
+					Character.toLowerCase(lletra)=='u') {
 				System.out.print(" VOCAL");
 			} else System.out.print(" CONSONANT");
 			
-			if (lletra.matches("^\\d+(\\.\\d+)?")) {
+			if (Character.isDigit(lletra)) {
 				System.out.print(" Els noms de persona no contenen números\n");
 			} else System.out.print("\n");
 		}
